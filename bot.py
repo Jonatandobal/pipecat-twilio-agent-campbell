d5fee9fec0c58c9f4cbfe18cfb93702943c8a221
Jonatandobal <128140189+Jonatandobal@users.noreply.github.com>
2025-05-22 15:23:57 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,14 +55,14 @@ async def save_audio(server_name: str, audio: bytes, sample_rate: int, num_chann
 
 
 async def run_bot(websocket_client: WebSocket, stream_sid: str, testing: bool):
-transport = FastAPIWebsocketTransport(
-    websocket=websocket_client,
-    params=FastAPIWebsocketParams(
-        audio_in_enabled=True,
-        audio_out_enabled=True,
-        add_wav_header=False,
-        vad_enabled=True,
-        vad_analyzer=SileroVADAnalyzer(
+        transport = FastAPIWebsocketTransport(
+            websocket=websocket_client,
+            params=FastAPIWebsocketParams(
+            audio_in_enabled=True,
+            audio_out_enabled=True,
+            add_wav_header=False,
+            vad_enabled=True,
+            vad_analyzer=SileroVADAnalyzer(
             min_speech_duration_ms=50,
             min_silence_duration_ms=300,
             speech_pad_ms=100
